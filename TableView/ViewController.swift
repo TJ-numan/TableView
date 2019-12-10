670b266d97e71f2f8af3ffc5421540f9fb26bc58
Abdullah Al Numan <abdullahalnuman235@gmail.com>
2019-12-09 22:13:32 -0800

    table view add and delete practice

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -8,12 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    var arr = ["Delight Cookie","Halwa Bun",]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell
+        cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text = "All time \(arr[indexPath.row])"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            arr.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.automatic)
+        }
+    }
+    
+    @IBOutlet weak var table: UITableView!
+    
+    @IBOutlet weak var Editor: UITextField!
+    
+    @IBAction func add(_ sender: Any) {
+        
+        arr.append(Editor.text!)
+        table.reloadData()
+        Editor.text = nil
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
